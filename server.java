0dd00a72564d66280dc5c403751ce0900f0bcb21
kimkaram <47126446+kimkaram@users.noreply.github.com>
2019-02-13 08:15:07 +0900

    TCP/IP 서버프로그램 수정
    여러개의 쓰레드를 생성해 클라이언트의 요청을 동시에 처리하도록 만듦.

--- server.java
+++ server.java
@@ -1,49 +1,73 @@
-package cp1;
-
+import java.util.*;
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
-public class Server {
+public class ThreadServer implements Runnable {
+
+	ServerSocket serverSocket;
+	Thread[] threadArr;
 
 	public static void main(String[] args) {
-		ServerSocket serverSocket = null;
+		// 5개의 쓰레드를 생성하는 서버를 생성
+		ThreadServer server = new ThreadServer(5);
+		server.start();
+	}
 
+	public ThreadServer(int i) {
 		try {
-			// 서버소켓을 생성해 7777번 포트와 결합시킨다.
+			// 서버소켓을 생성해 777번 포트와 결합시킨다.
 			serverSocket = new ServerSocket(7777);
 			System.out.println(getTime() + "서버가 준비되었습니다.");
+
+			threadArr = new Thread[i];
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
 
-		while(true) {
+	private void start() {
+		for (int i = 0; i < threadArr.length; i++) {
+			threadArr[i] = new Thread(this);
+			threadArr[i].start();
+		}
+
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			// 서버소켓
 			try {
-				System.out.println(getTime() + "연결요청을기다립니다.");
-				// 서버소켓은 클라이언트의 연결요청이 올 때까지 실행을 멈추고 계속 기다린다.
-				// 클라이언트의 연결 요청이 오면 클라이언트 소켓과 통신할 새로운 소켓을 생성한다.
+				System.out.println(getTime() + "연결요청을 기다립니다.");
+
 				Socket socket = serverSocket.accept();
 				System.out.println(getTime() + socket.getInetAddress() + "로부터 연결요청이 들어왔습니다.");
+				System.out.println("getPort() : " + socket.getPort());
+				System.out.println("getLocalPort() : " + socket.getLocalPort());
 
 				// 소켓의 출력스트림을 얻는다.
 				OutputStream out = socket.getOutputStream();
 				DataOutputStream dout = new DataOutputStream(out);
 
-				// 원격소켓에 데이터를 보낸다.
+				// 원격 소켓에 데이터를 보낸다.
 				dout.writeUTF("[Notice] Test Message1 from Server");
 				System.out.println(getTime() + "데이터를 전송했습니다.");
 
-				// 스트림과 소켓을 닫아준다.
+				// 스트림과 소켓 닫아준다.
 				dout.close();
 				socket.close();
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
+
 		}
+
 	}
 
-	// 현재시간을 문자열로 반환하는 함수
+	// 현재 시간을 문자열로 변환해주는 메소드
 	private static String getTime() {
 		SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
 		return f.format(new Date());
